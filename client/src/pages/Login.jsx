3d9f703c092353bd092a8ea9a6e4c658786430e6
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-05-16 17:59:17 +0530

    Routing, Login & Register form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/LoginStyles.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  // form handler
+  const onfinishHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onfinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Login Form</h3>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to="/register" className="m-2">
+            Not a user Register here
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </Form>
+      </div>
+    </>
   );
 };
 
