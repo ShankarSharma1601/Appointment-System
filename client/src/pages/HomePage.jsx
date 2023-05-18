89393d168ce52acb1a106f9507929c7bc4184a34
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-05-18 13:49:20 +0530

    Layout Setup

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import Layout from "../components/Layout";
 
 const HomePage = () => {
   // login user data
@@ -23,9 +24,9 @@ const HomePage = () => {
     getUserData;
   }, []);
   return (
-    <div>
+    <Layout>
       <h1>Homepage</h1>
-    </div>
+    </Layout>
   );
 };
 
